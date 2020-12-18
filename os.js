4f991808055a6c776e0444d19bc71ebc143bc2b9
wonjinsong <msmk22239@gmail.com>
2020-12-17 19:06:29 -0500

    path

--- os.js
+++ os.js
@@ -20,3 +20,109 @@ console.log("os.cpus().length:", os.cpus().length);
 console.log("memory info-------------------");
 console.log("os.freemem():", os.freemem());
 console.log("os.totalmem():", os.totalmem());
+
+// Os info -------------------
+// os.arch(): x64
+// os.platform(): win32
+// os.type(): Windows_NT
+// os.uptime(): 631694
+// os.hostname(): LAPTOP-M08K4NQB
+// os.release(): 10.0.18363
+// path info ------------------
+// os.homedir(): C:\Users\msmk2
+// os.tmpdir(): C:\Users\msmk2\AppData\Local\Temp
+// cpu info --------------------
+// os.cpus(): [
+//   {
+//     model: 'Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz',
+//     speed: 1800,
+//     times: {
+//       user: 5488203,
+//       nice: 0,
+//       sys: 8341687,
+//       idle: 112762718,
+//       irq: 1124875
+//     }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz',
+//     speed: 1800,
+//     times: {
+//       user: 3796890,
+//       nice: 0,
+//       sys: 4011453,
+//       idle: 118783796,
+//       irq: 58140
+//     }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz',
+//     speed: 1800,
+//     times: {
+//       user: 9289843,
+//       nice: 0,
+//       sys: 7169281,
+//       idle: 110132968,
+//       irq: 101562
+//     }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz',
+//     speed: 1800,
+//     times: {
+//       user: 5005015,
+//       nice: 0,
+//       sys: 4282750,
+//       idle: 117304375,
+//       irq: 48984
+//     }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz',
+//     speed: 1800,
+//     times: {
+//       user: 8286203,
+//       nice: 0,
+//       sys: 6942828,
+//       idle: 111363078,
+//       irq: 94343
+//     }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz',
+//     speed: 1800,
+//     times: {
+//       user: 5099093,
+//       nice: 0,
+//       sys: 7045796,
+//       idle: 114447234,
+//       irq: 61781
+//     }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz',
+//     speed: 1800,
+//     times: {
+//       user: 6880015,
+//       nice: 0,
+//       sys: 6305687,
+//       idle: 113406375,
+//       irq: 73390
+//     }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz',
+//     speed: 1800,
+//     times: {
+//       user: 5510937,
+//       nice: 0,
+//       sys: 6824953,
+//       idle: 114256234,
+//       irq: 51421
+//     }
+//   }
+// ]
+// os.cpus().length: 8
+// memory info-------------------
+// os.freemem(): 2976161792
+// os.totalmem(): 8463486976
---
+++ path.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+const string = __filename;
+
+console.log("path.sep:", path.sep);
+console.log("path.delimiter:", path.delimiter);
+
+console.log("------------------------");
+console.log("path.dirname():", path.dirname(string));
+console.log("path.extname():", path.extname(string));
+console.log("path.basename():", path.basename(string));
+console.log(
+  "path.basename - extname:",
+  path.basename(string, path.extname(string))
+);
+
+console.log("------------------------");
+console.log("path.parse():", path.parse(string));
+console.log(
+  "path.format():",
+  path.format({
+    dir: "C:\\Users\\msmk2\\OneDrive\\Documents\\better2\\nodejs",
+    name: "path",
+    ext: "js",
+  })
+);
+console.log(
+  "path.normalize():",
+  path.normalize("C://Users\\\\msmk2\\OneDrive\\Documents\\\\better2\nodejs")
+);
+
+console.log("------------------------");
+console.log("path.isAbsolute(C:\\):", path.isAbsolute("c:\\"));
+console.log("path.isAbsolute(./home):", path.isAbsolute("./home"));
+
+console.log("------------------------");
+console.log(
+  "path.relative():",
+  path.relative(
+    "C:\\Users\\msmk2\\OneDrive\\Documents\\better2\\nodejs\\path.js",
+    "c:\\"
+  )
+);
